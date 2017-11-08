2ad20634e0ffb29858b3745b62858695622614b7
Microshaoft <microshaoft@msn.com>
2017-11-08 16:47:10 +0800

    allBindingsAccessor

--- DurandalKnockoutSPA/lib/knockout/customBindings/format.js
+++ DurandalKnockoutSPA/lib/knockout/customBindings/format.js
@@ -1,11 +1,14 @@
 ﻿define(["knockout"],function (ko1) {
     ko1.bindingHandlers.format = {
-
-        update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
-            if (viewModel.compositionCompleted) {
-                debugger;
-            } else {
-                debugger;
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+            var bindingFieldValueAccessor = allBindingsAccessor.get('value');
+            var bindingFieldValue = bindingFieldValueAccessor();
+            var params = valueAccessor();
+            //console.log(params.Param1);
+            //console.log(params.Param2);
+            if (!viewModel.compositionCompleted) {
+                //仅在 compositionCompleted 之前处理一次
+                bindingFieldValueAccessor("CompositionCompleted: " + bindingFieldValue);
             }
         }
     };
